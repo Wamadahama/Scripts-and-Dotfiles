57db0c273df05382375b18664d1616307b3570b7
Elijah Ellis <smurftown2@gmail.com>
2016-04-21 21:06:43 -0700

    Updated setscreen.sh: Fixed Typo

--- Scripts/Tools/setscreen.sh
+++ Scripts/Tools/setscreen.sh
@@ -2,4 +2,4 @@
 # it just werks XD 
 xrandr --newmode "1920x1080" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode Virtual1 1920x1080
-xrandr --output Virtual1 --mode 1920x108
+xrandr --output Virtual1 --mode 1920x1080
