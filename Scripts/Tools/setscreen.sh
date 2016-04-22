c82bebcf12b5723a4176dd534f6c9e8e239a338a
Elijah Ellis <Elijah Ellis>
2016-04-21 22:29:14 -0400

    comment

--- Scripts/Tools/setscreen.sh
+++ Scripts/Tools/setscreen.sh
@@ -1,3 +1,5 @@
+# Virtualizing ubuntu with a 1920x1080 is impossible without this
+# it just werks XD 
 xrandr --newmode "1920x1080" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode Virtual1 1920x1080
 xrandr --output Virtual1 --mode 1920x108
